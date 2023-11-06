528b6316a49570eba624a73b51adbf65d4785f66
Rana Amir Bashir <rana.amir.bashir@redmath.com>
2023-11-06 15:36:54 +0500

    table and data created via liquibase

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO RESTAURANTS VALUES (1, 'Delhi Belly', 'Fast Food', 'Good', 'Not available', 'Burgers', '25', '12', '23'); 
-
-insert into restaurant(id, name, restauranttype, rating, logo, cusinetype, seatingcapacity, opentime, closetime  )  values (1, 'Hardees', 'Fast Food', '4', '', 'Burgers', '100', '8PM', '2 AM'  )
-commit;
